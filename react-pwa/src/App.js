e7f6ed79a534c50a792bb7b926705759b3baca3d
d21emijo <102797583+d21emijo@users.noreply.github.com>
2025-04-03 10:20:46 +0200

    Css similar to vue
    Same css, remove header for now

--- react-pwa/src/App.js
+++ react-pwa/src/App.js
@@ -12,7 +12,7 @@ function App() {
   return (
     
     <div className="App">
-      <Header > {Header}</Header>
+      {/* <Header > {Header}</Header> */}
       <CategoryList /> {/*  */}
 
     </div>
